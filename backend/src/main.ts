d2017e9be73383101f587c3200b663666b55d14a
DjaouedKH <74308036+djaouedkh@users.noreply.github.com>
2024-10-27 18:01:00 +0100

    Prod (#4)
    * jenkinsfile init (#3)
    
    * ci cd

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
